2caf4850f384292a7858f85b6d85212cf73d81a9
Savin Angel-Mario <hello@m4.rocks>
2025-04-28 12:31:11 +0000

    feat: add Cloudflare adapter to Astro configuration and update dependencies

--- packages/frontend/astro.config.mjs
+++ packages/frontend/astro.config.mjs
@@ -4,15 +4,20 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
 	output: "static",
+
 	server: {
 		port: 3000,
 	},
+
 	vite: {
 		plugins: [tailwindcss()],
 	},
 
 	integrations: [react()],
+	adapter: cloudflare(),
 });
