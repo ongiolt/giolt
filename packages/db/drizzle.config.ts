6f27e584020a3e69abd339e5ce3cb7a945d9b328
Savin Angel-Mario <hello@m4.rocks>
2025-05-19 13:32:58 +0000

    feat(db): add db:generate script and improve formatting in configuration files

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
 	dialect: "sqlite",
-	schema: "./src/schema.ts"
-})
+	schema: "./src/schema.ts",
+});
--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -3,5 +3,9 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const users = sqliteTable("users", {
 	id: integer("id").primaryKey().unique(),
 	customerId: integer("customer_id").notNull(),
-	subscriptionStatus: text("subscription_status", { enum: ["active", "inactive"] }).notNull().default("inactive")
-})
+	subscriptionStatus: text("subscription_status", {
+		enum: ["active", "inactive"],
+	})
+		.notNull()
+		.default("inactive"),
+});
