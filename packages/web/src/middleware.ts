d692e44709121bb5ece0714dcc2722d86a6d8c26
Savin Angel-Mario <hello@m4.rocks>
2025-05-20 07:34:27 +0000

    feat(db): add wrangler integration and update drizzle configuration for local database management

--- packages/web/src/middleware.ts
+++ packages/web/src/middleware.ts
@@ -1,26 +1,10 @@
 import { clerkMiddleware } from "@clerk/astro/server";
 import { defineMiddleware, sequence } from "astro/middleware";
 import { Stripe } from "stripe";
-import { getStripeCustomer } from "./lib/stripe";
 
 export const stripeMiddleware = defineMiddleware(async (ctx, next) => {
-	const { userId } = ctx.locals.auth();
 	const stripe = new Stripe(ctx.locals.runtime.env.STRIPE_SECRET_KEY);
-
-	ctx.locals.customerId = null;
 	ctx.locals.stripe = stripe;
-
-	if (!userId) {
-		return next();
-	}
-
-	const customerId = await getStripeCustomer(ctx, userId);
-
-	if (!customerId) {
-		return next();
-	}
-
-	ctx.locals.customerId = customerId;
 
 	return next();
 });
