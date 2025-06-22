f7566be091ce4d60e0504ed416720716949ce7c5
Savin Angel-Mario <hello@m4.rocks>
2025-06-22 19:58:41 +0300

    Added db

---
+++ packages/db/src/schema.ts
@@ -0,0 +1,10 @@
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { THEMES } from "./data";
+
+export const pages = sqliteTable("pages", {
+	id: text("id").primaryKey().unique().notNull(),
+	name: text("name").notNull(),
+	description: text("description"),
+	avatarUrl: text("logo_url"),
+	theme: text("theme", { enum: THEMES }).notNull().default("light")
+})
