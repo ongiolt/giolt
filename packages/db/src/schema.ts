2ed981eb912760a8b436385da138d21f919d8aed
Savin Angel-Mario <hello@m4.rocks>
2025-06-23 13:05:33 +0000

    Refactor: Rename db package to giolt-db and update references; add products table to schema

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,4 +1,4 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { THEMES } from "./data";
 
 export const pages = sqliteTable("pages", {
@@ -7,4 +7,15 @@ export const pages = sqliteTable("pages", {
 	description: text("description"),
 	avatarUrl: text("logo_url"),
 	theme: text("theme", { enum: THEMES }).notNull().default("light"),
+	products: int("products").references(() => products.id),
+	public: int("public", { mode: "boolean" }).notNull().default(false),
+});
+
+export const products = sqliteTable("products", {
+	id: int("id").primaryKey().unique().notNull(),
+	name: text("name").notNull(),
+	description: text("description"),
+	imageUrl: text("image_url"),
+	price: int("price").notNull(),
+	checkoutLink: text("checkout_link").notNull(),
 });
