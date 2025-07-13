b6953f5fa857196228c0022b28d6e09ba609aaef
Savin Angel-Mario <hello@m4.rocks>
2025-07-13 12:09:33 +0300

    Code quality fix

--- src/lib/maps.ts
+++ src/lib/maps.ts
@@ -3,7 +3,9 @@ export const getWazeUrl = (location: string): string => {
 };
 
 export const getGoogleMapsUrl = (location: string): string => {
-	return encodeURI(`https://www.google.com/maps/search/?api=1&query=${location}`);
+	return encodeURI(
+		`https://www.google.com/maps/search/?api=1&query=${location}`,
+	);
 };
 
 export const getAppleMapsUrl = (location: string): string => {
@@ -11,17 +13,19 @@ export const getAppleMapsUrl = (location: string): string => {
 };
 
 export const getMapsImage = async (location: string): Promise<string> => {
-	const url = encodeURI(`https://maps.googleapis.com/maps/api/staticmap?center=${location}&zoom=17&size=400x300&scale=2&markers=size:mid|color:red|${location}&key=${import.meta.env.GOOGLE_MAPS_API_KEY}`);
+	const url = encodeURI(
+		`https://maps.googleapis.com/maps/api/staticmap?center=${location}&zoom=17&size=400x300&scale=2&markers=size:mid|color:red|${location}&key=${import.meta.env.GOOGLE_MAPS_API_KEY}`,
+	);
 
 	return await signMapsStaticURL(url, import.meta.env.GOOGLE_MAPS_SECRET);
-}
+};
 
 function removeWebSafe(safeEncodedString: string): string {
-	return safeEncodedString.replace(/-/g, '+').replace(/_/g, '/');
+	return safeEncodedString.replace(/-/g, "+").replace(/_/g, "/");
 }
 
 function makeWebSafe(encodedString: string): string {
-	return encodedString.replace(/\+/g, '-').replace(/\//g, '_');
+	return encodedString.replace(/\+/g, "-").replace(/\//g, "_");
 }
 
 function decodeBase64Hash(code: string): Uint8Array {
@@ -33,22 +37,34 @@ function decodeBase64Hash(code: string): Uint8Array {
 	return bytes;
 }
 
-async function encodeBase64Hash(key: Uint8Array, data: string): Promise<string> {
+async function encodeBase64Hash(
+	key: Uint8Array,
+	data: string,
+): Promise<string> {
 	const encoder = new TextEncoder();
 	const cryptoKey = await crypto.subtle.importKey(
-		'raw',
+		"raw",
 		key,
-		{ name: 'HMAC', hash: 'SHA-1' },
+		{ name: "HMAC", hash: "SHA-1" },
 		false,
-		['sign']
+		["sign"],
 	);
-	const signature = await crypto.subtle.sign('HMAC', cryptoKey, encoder.encode(data));
+	const signature = await crypto.subtle.sign(
+		"HMAC",
+		cryptoKey,
+		encoder.encode(data),
+	);
 	return btoa(String.fromCharCode(...new Uint8Array(signature)));
 }
 
-async function signMapsStaticURL(path: string, secret: string): Promise<string> {
+async function signMapsStaticURL(
+	path: string,
+	secret: string,
+): Promise<string> {
 	const uri = new URL(path);
 	const safeSecret = decodeBase64Hash(removeWebSafe(secret));
-	const hashedSignature = makeWebSafe(await encodeBase64Hash(safeSecret, uri.pathname + uri.search));
-	return uri.toString() + '&signature=' + hashedSignature;
+	const hashedSignature = makeWebSafe(
+		await encodeBase64Hash(safeSecret, uri.pathname + uri.search),
+	);
+	return `${uri.toString()}&signature=${hashedSignature}`;
 }
